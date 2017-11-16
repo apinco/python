03e9852583d74492136b1a3a085dad8487d524e7
apfeiffe <TanRie20>
2017-11-16 10:42:43 -0700

    All files on work computer

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,25 +1,43 @@
-# Import needed modules
 import json
-# Functions
-def find_def(word):
-    if (word in data):
-        return str(data[word])
+import difflib
+from difflib import get_close_matches
+
+def load_dict():
+    dict = json.load(open("data.json"))
+    return dict
+
+def get_input_word():
+    word = input("What word do you want to look up? ")
+    return word
+
+def find_close_match(dict,word):
+    matches = get_close_matches(word, dict.keys())
+    if matches:
+        word = input("Found the following close matches, type the word you really want" + str(matches))
+        return word
     else:
-        print("Word doesn't exist")
-        exit()
+        print("found no close match")
+        return ""
 
-# Read data into a dictionary (which is really just a hash)
-data = json.load(open("data.json"))
-
-# Perl like looping
-#for key in data:
-#    print("key: " + str(key) + " value: " + str(data[key]))
-
-#Python style loop, must more efficient that the above method
-#for key, value in data.items():
-#    print("key:" + str(key) + "value:" + str(value))
-
-word = input("Enter a word to look up: ")
-print(find_def(word))
+def print_def(definitions):
+    i = 1
+    for definition in definitions:
+        print("Definition " + str(i)+ ":" + definition)
+        i=i+1
 
 
+def get_def(dict, word):
+    if word.lower() in dict:
+        print_def(dict[word])
+    elif word.title() in dict:
+        print_def(dict[word.title()])
+    else:
+        close_match = find_close_match(dict,word)
+        if len(close_match) > 0:
+            print_def(dict[close_match])
+        else:
+            print("didn't find the word")
+
+dict = load_dict()
+word = get_input_word()
+get_def(dict,word)
